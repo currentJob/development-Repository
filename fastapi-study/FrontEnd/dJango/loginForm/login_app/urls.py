80a93322aba896a950d3cd226a27e847744e0798
currentJob <an8957@gmail.com>
2025-02-07 16:41:45 +0900

    🚑️ Del

--- fastapi-study/FrontEnd/dJango/loginForm/login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'login_app'  # namespacing 추가
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('home/', views.home_view, name='home'),
-]
